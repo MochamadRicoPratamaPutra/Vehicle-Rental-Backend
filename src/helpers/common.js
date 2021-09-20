56b42739057bc1ce9fee256dd543728a07c4d9dc
MochamadRicoPratamaPutra <mricopratamaputra@gmail.com>
2021-09-20 19:39:36 +0700

    fixing nodemailer

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -1,15 +1,17 @@
 const nodemailer = require('nodemailer');
 function main(name, email, id) {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: process.env.COFFEESHOP_HOST,
+    port: process.env.COFFEESHOP_PORT,
+    secure: process.env.COFFEESHOP_SECURE,
     auth: {
-      user: 'ricoxxx007@gmail.com',
-      pass: 'cangcimeng007',
+      user: `${process.env.EMAIL_MAILER}`,
+      pass: `${process.env.EMAIL_PASSWORD}`,
     },
   });
   transporter
     .sendMail({
-      from: '"Circle"<ricoxxx007@gmail.com>',
+      from: `"Circle"<${process.env.EMAIL_MAILER}>`,
       to: `${email}`,
       subject: 'Thank you for registering to Circle!',
       html: `<h style:'margin-left:auto; margin-right:auto'>Thank you ${name} for registering to Circle</h><p style:'margin-left:auto; margin-right:auto'>One more step to awesome riding experience!</p><button style:'margin-left:auto; margin-right:auto;outline:none;border-radius:3px; background: red;padding: 5px 10px'><a href='${process.env.TARGET_URL}/verification/${id}'>Click here to confirm your account!</a></button>`,
